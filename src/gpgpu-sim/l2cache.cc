ff794d64ec740c2b92e41f27e44eab0c64c37612
Zhukun Wang <zhukun.wang@chch.ox.ac.uk>
2025-05-21 04:03:00 +0000

    First Update

--- src/gpgpu-sim/l2cache.cc
+++ src/gpgpu-sim/l2cache.cc
@@ -553,6 +553,64 @@ void memory_sub_partition::cache_cycle(unsigned cycle) {
             m_icnt_L2_queue->pop();
           }
         } else if (status != RESERVATION_FAIL) {
+
+                if (status == MISS){
+
+                    FILE* f = fopen("memory_access.txt", "a");
+
+                    new_addr_type probe_pointer = mf->get_addr();
+                    enum mem_access_type type = mf->get_access_type();
+                    if (m_gpu->gpu_tot_sim_cycle > m_gpu->gpu_sim_cycle){
+                        fprintf(f, "Time: %6llu cycles  %6.3f us || ", m_gpu->gpu_tot_sim_cycle, 1.428570*0.001*m_gpu->gpu_tot_sim_cycle);
+                    }
+                    else{
+                        fprintf(f, "Time: %6llu cycles  %6.3f us || ", m_gpu->gpu_sim_cycle, 1.42857*0.001*m_gpu->gpu_sim_cycle);
+                    }
+                    switch (type) {
+                        case GLOBAL_ACC_R:
+                                fprintf(f, "Access Type:  Read Global Memory     || ");
+                                break;
+                        case LOCAL_ACC_R:
+                                fprintf(f, "Access Type:  Read Local Memory      || ");
+                                break;
+                        case CONST_ACC_R:
+                                fprintf(f, "Access Type:  Read Constant Cache    || ");
+                                break;
+                        case TEXTURE_ACC_R:
+                                fprintf(f, "Access Type:  Read Texture Cache     || ");
+                                break;
+                        case GLOBAL_ACC_W:
+                                fprintf(f, "Access Type:  Write Global Memory    || ");
+                                break;
+                        case LOCAL_ACC_W:
+                                fprintf(f, "Access Type:  Write Local Memory     || ");
+                                break;
+                        case L1_WRBK_ACC:
+                                fprintf(f, "Access Type:  L1 Cache Write Back    || ");
+                                break;
+                        case L2_WRBK_ACC:
+                                fprintf(f, "Access Type:  L2 Cache Write Back    || ");
+                                break;
+                        case INST_ACC_R:
+                                fprintf(f, "Access Type:  Read Instruction Cache || ");
+                                break;
+                        case L1_WR_ALLOC_R:
+                                fprintf(f, "Access Type:  L1 Write-Allocate Read || ");
+                                break;
+                        case L2_WR_ALLOC_R:
+                                fprintf(f, "Access Type:  L2 Write-Allocate Read || ");
+                                break;
+                        case NUM_MEM_ACCESS_TYPE:
+                                fprintf(f, "Access Type:  NUM_MEM_ACCESS_TYPE    || ");
+                                break;
+                        default:
+                                fprintf(f, "Access Type:  UNKNOWN                || ");
+                                break;
+                    }
+                    fprintf(f, "Access Address:  %llx \n", probe_pointer);
+                    fclose(f);
+           }
+
           if (mf->is_write() &&
               (m_config->m_L2_config.m_write_alloc_policy == FETCH_ON_WRITE ||
                m_config->m_L2_config.m_write_alloc_policy ==
